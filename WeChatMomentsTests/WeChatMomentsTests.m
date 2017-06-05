7ff0706b9c9c79950a5ce6fe19834a610d4a5600
quanbowei <quanbowei2008@126.com>
2017-06-05 20:17:55 +0800

    unit test ok

--- WeChatMomentsTests/WeChatMomentsTests.m
+++ WeChatMomentsTests/WeChatMomentsTests.m
@@ -11,6 +11,7 @@
 
 #import "QBCache.h"
 NSString *kImageTestKey = @"TestImageKey.jpg";
+NSTimeInterval kAsyncTestTimeout = 2;
 
 @interface WeChatMomentsTests : XCTestCase
 
@@ -22,6 +23,7 @@ NSString *kImageTestKey = @"TestImageKey.jpg";
 - (void)setUp {
     [super setUp];
     self.sharedCache = [QBCache sharedCache];
+    [self clearAllCaches];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 - (void)test01SharedCache {
@@ -48,16 +50,16 @@ NSString *kImageTestKey = @"TestImageKey.jpg";
             }
         }];
     }];
-    [self waitForExpectationsWithTimeout:2 handler:nil];
+    [self waitForExpectationsWithTimeout:kAsyncTestTimeout handler:nil];
 }
 - (void)test06InsertionOfImage {
     XCTestExpectation *expectation = [self expectationWithDescription:@"storeImage forKey"];
     
     UIImage *image = [self imageForTesting];
     NSData *imageData = [self imageDataForTesting];
+    
     [self.sharedCache setObject:image withImageData:imageData forKey:kImageTestKey];
     expect([self.sharedCache.memoryCache objectForKey:kImageTestKey]).to.equal(image);
-    
     [self.sharedCache.diskCache  containsObjectForKey:kImageTestKey withBlock:^(NSString *key, BOOL contains) {
         
         if (contains) {
@@ -66,9 +68,67 @@ NSString *kImageTestKey = @"TestImageKey.jpg";
             XCTFail(@"Image should be in cache");
         }
     }];
-    [self waitForExpectationsWithTimeout:2 handler:nil];
+    [self waitForExpectationsWithTimeout:kAsyncTestTimeout handler:nil];
     
 }
+- (void)test07InsertionOfImageToDisk{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"storeImage forKey toDisk"];
+
+    [self.sharedCache setObject:nil withImageData:[self imageDataForTesting] forKey:kImageTestKey];
+    expect([self.sharedCache.memoryCache objectForKey:kImageTestKey]).to.beNil();
+    [self.sharedCache.diskCache containsObjectForKey:kImageTestKey withBlock:^(NSString *key, BOOL contains) {
+        if (contains) {
+            [expectation fulfill];
+        } else {
+            XCTFail(@"Image should be in cache");
+        }
+    }];
+    [self waitForExpectationsWithTimeout:kAsyncTestTimeout handler:nil];
+}
+- (void)test08InsertionOfImageToMem{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"storeImage forKey mem only"];
+    
+    UIImage * image = [self imageForTesting];
+    [self.sharedCache setObject:image withImageData:nil forKey:kImageTestKey];
+    expect([self.sharedCache objectForKey:kImageTestKey]).to.equal(image);
+    [self.sharedCache.diskCache containsObjectForKey:kImageTestKey withBlock:^(NSString *key, BOOL contains) {
+        if (!contains) {
+            [expectation fulfill];
+        } else {
+            XCTFail(@"Image should be in cache");
+        }
+    }];
+    [self waitForExpectationsWithTimeout:kAsyncTestTimeout handler:nil];
+}
+- (void)test09RemoveImage{
+    UIImage *image = [self imageForTesting];
+    NSData *imageData = [self imageDataForTesting];
+    [self.sharedCache setObject:image withImageData:imageData forKey:kImageTestKey];
+    [self.sharedCache removeObjectForKey:kImageTestKey withBlock:^(NSString *key) {
+        expect([self.sharedCache.memoryCache objectForKey:kImageTestKey]).to.beNil;
+        expect([self.sharedCache.diskCache objectForKey:kImageTestKey]).to.beNil;
+    }];
+}
+- (void)test10RemoveImageFromMem{
+    UIImage *image = [self imageForTesting];
+    NSData *imageData = [self imageDataForTesting];
+    [self.sharedCache setObject:image withImageData:imageData forKey:kImageTestKey];
+    [self.sharedCache.memoryCache removeObjectForKey:kImageTestKey];
+    expect([self.sharedCache.memoryCache objectForKey:kImageTestKey]).to.beNil;
+    expect([self.sharedCache.diskCache objectForKey:kImageTestKey]).toNot.beNil;
+}
+
+- (void)test11RemoveImageFromDisk{
+    UIImage *image = [self imageForTesting];
+    NSData *imageData = [self imageDataForTesting];
+    [self.sharedCache setObject:image withImageData:imageData forKey:kImageTestKey];
+    expect([self.sharedCache objectForKey:kImageTestKey]).to.equal(image);
+    [self.sharedCache.diskCache containsObjectForKey:kImageTestKey withBlock:^(NSString *key, BOOL contains) {
+        expect([self.sharedCache.diskCache objectForKey:kImageTestKey]).to.beNil;
+    }];
+}
+
+
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
@@ -85,7 +145,9 @@ NSString *kImageTestKey = @"TestImageKey.jpg";
         // Put the code you want to measure the time of here.
     }];
 }
-
+- (void)clearAllCaches{
+    [self.sharedCache removeAllObjects];
+}
 
 - (UIImage *)imageForTesting{
     static UIImage *reusableImage = nil;
